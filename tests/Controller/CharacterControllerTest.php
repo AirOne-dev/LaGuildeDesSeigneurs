67122089c114e734d51d5b6d1a53964f9698cf6e
Firologame <erwan1207@gmail.com>
2022-02-07 10:20:03 +0100

    change tests

--- tests/Controller/CharacterControllerTest.php
+++ tests/Controller/CharacterControllerTest.php
@@ -6,12 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CharacterControllerTest extends WebTestCase
 {
-    public function testCharacterDisplay(): void
+    public function testDisplay(): void
     {
         $client = static::createClient();
         $client->request('GET', '/character/display');
 
-        $response = $client->getResponse();
+        $this->assertJsonResponse($client->getResponse());
+    }
+
+    public function assertJsonResponse($response): void
+    {
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertTrue($response->headers->contains('Content-Type', 'application/json'), $response->headers);
     }
