9289c8ca82a9876c15e75db1fdbf1648c0d7159c
Firologame <erwan1207@gmail.com>
2022-03-15 09:00:21 +0100

    add api html view

---
+++ src/Controller/CharacterApiHtmlController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Character;
+use App\Form\CharacterApiHtmlType;
+use App\Form\CharacterHtmlType;
+use App\Repository\CharacterRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use App\Service\CharacterServiceInterface;
+
+/**
+ * @Route("/character/api-html")
+ */
+class CharacterApiHtmlController extends AbstractController
+{
+    private $characterService;
+
+    public function __construct(CharacterServiceInterface $characterService)
+    {
+        $this->characterService = $characterService;
+    }
+
+    #[Route('/', name: 'character_api_html_index', methods: ['GET'])]
+    public function index(CharacterRepository $characterRepository): Response
+    {
+        return $this->render('character_api_html/index.html.twig', [
+            'characters' => $characterRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'character_api_html_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('characterCreate', null);
+
+        $character = new Character();
+        $form = $this->createForm(CharacterApiHtmlType::class, $character);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->characterService->createFromHtml($character);
+
+            return $this->redirectToRoute('character_api_html_show', array(
+                'identifier' => $character->getIdentifier(),
+            ));
+        }
+
+        return $this->renderForm('character_api_html/new.html.twig', [
+            'character' => $character,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{identifier}', name: 'character_api_html_show', methods: ['GET'])]
+    public function show(Character $character): Response
+    {
+        return $this->render('character_html/show.html.twig', [
+            'character' => $character,
+        ]);
+    }
+
+    #[Route('/{identifier}/edit', name: 'character_api_html_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Character $character): Response
+    {
+        $this->denyAccessUnlessGranted('characterModify', $character);
+
+        $form = $this->createForm(CharacterApiHtmlType::class, $character);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->characterService->modifyFromHtml($character);
+
+            return $this->redirectToRoute('character_api_html_show', array(
+                'identifier' => $character->getIdentifier(),
+            ));
+        }
+
+        return $this->renderForm('character_api_html/edit.html.twig', [
+            'character' => $character,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{identifier}/delete', name: 'character_api_html_delete', methods: ['GET', 'POST'])]
+    public function delete(Request $request, Character $character): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$character->getIdentifier(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($character);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('character_api_html_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/CharacterApiHtmlType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Character;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CharacterApiHtmlType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, array(
+            'required' => false,
+            'label' => 'Nom'
+        ))
+            ->add('surname', TextType::class)
+            ->add('kind', TextType::class)
+            ->add('caste', TextType::class, array(
+                'required' => false,
+                'help' => 'Caste du Character',
+            ))
+            ->add('knowledge', TextType::class, array(
+                'required' => false,
+            ))
+            ->add('intelligence', IntegerType::class, array(
+                'required' => false,
+                'help' => 'Niveau d\'intelligence du Character (1-250)',
+                'attr' => array(
+                    'min' => 1,
+                    'max' => 250,
+                ),
+            ))
+            ->add('life', IntegerType::class, array(
+                'required' => false,
+                'label' => 'Niveau de vie',
+                'attr' => array(
+                    'min' => 1,
+                    'max' => 250,
+                    'placeholder' => 'Niveau de vie du Character (1-250)',
+                ),
+            ))
+            ->add('image', TextType::class, array(
+                'required' => false,
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Character::class,
+        ]);
+    }
+}
